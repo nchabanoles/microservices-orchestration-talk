817461fbf311b6f433be1e172279638d71c59c19
Nicolas Chabanoles <nicolas.chabanoles@bonitasoft.com>
2019-11-06 18:13:16 +0100

    tr(kafka): delete un-used send message code

--- bonita-kafka-watcher/src/main/java/bonita/kafka/watcher/ReceiverConfig.java
+++ bonita-kafka-watcher/src/main/java/bonita/kafka/watcher/ReceiverConfig.java
@@ -31,7 +31,7 @@ public class ReceiverConfig {
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
                 StringDeserializer.class);
         // allows a pool of processes to divide the work of consuming and processing records
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "helloworld");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "bonita-kafka");
         // automatically reset the offset to the earliest offset
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
